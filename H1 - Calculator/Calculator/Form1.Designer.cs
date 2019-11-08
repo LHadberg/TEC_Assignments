12ee09cd2f35ff2a7208f3dd09a57101d48a4191
LHadberg <54304968+LHadberg@users.noreply.github.com>
2019-11-08 11:04:03 +0100

    Add files via upload

---
+++ H1 - Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Calculator
+{
+    partial class MainInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainInterface));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.MultiplyButton = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.CommaButton = new System.Windows.Forms.Button();
+            this.DivideButton = new System.Windows.Forms.Button();
+            this.SubtractButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.ClearEverythingButton = new System.Windows.Forms.Button();
+            this.ClearButton = new System.Windows.Forms.Button();
+            this.EqualsButton = new System.Windows.Forms.Button();
+            this.textBox_Result = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.TabStop = false;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.TabStop = false;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.TabStop = false;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.TabStop = false;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            resources.ApplyResources(this.button5, "button5");
+            this.button5.Name = "button5";
+            this.button5.TabStop = false;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            resources.ApplyResources(this.button6, "button6");
+            this.button6.Name = "button6";
+            this.button6.TabStop = false;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            resources.ApplyResources(this.button7, "button7");
+            this.button7.Name = "button7";
+            this.button7.TabStop = false;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            resources.ApplyResources(this.button8, "button8");
+            this.button8.Name = "button8";
+            this.button8.TabStop = false;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            resources.ApplyResources(this.button9, "button9");
+            this.button9.Name = "button9";
+            this.button9.TabStop = false;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // MultiplyButton
+            // 
+            resources.ApplyResources(this.MultiplyButton, "MultiplyButton");
+            this.MultiplyButton.Name = "MultiplyButton";
+            this.MultiplyButton.TabStop = false;
+            this.MultiplyButton.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            resources.ApplyResources(this.button0, "button0");
+            this.button0.Name = "button0";
+            this.button0.TabStop = false;
+            this.button0.UseVisualStyleBackColor = true;
+            // 
+            // CommaButton
+            // 
+            resources.ApplyResources(this.CommaButton, "CommaButton");
+            this.CommaButton.Name = "CommaButton";
+            this.CommaButton.TabStop = false;
+            this.CommaButton.UseVisualStyleBackColor = true;
+            // 
+            // DivideButton
+            // 
+            resources.ApplyResources(this.DivideButton, "DivideButton");
+            this.DivideButton.Name = "DivideButton";
+            this.DivideButton.TabStop = false;
+            this.DivideButton.UseVisualStyleBackColor = true;
+            // 
+            // SubtractButton
+            // 
+            resources.ApplyResources(this.SubtractButton, "SubtractButton");
+            this.SubtractButton.Name = "SubtractButton";
+            this.SubtractButton.TabStop = false;
+            this.SubtractButton.UseVisualStyleBackColor = true;
+            // 
+            // AddButton
+            // 
+            resources.ApplyResources(this.AddButton, "AddButton");
+            this.AddButton.Name = "AddButton";
+            this.AddButton.TabStop = false;
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // ClearEverythingButton
+            // 
+            resources.ApplyResources(this.ClearEverythingButton, "ClearEverythingButton");
+            this.ClearEverythingButton.Name = "ClearEverythingButton";
+            this.ClearEverythingButton.TabStop = false;
+            this.ClearEverythingButton.UseVisualStyleBackColor = true;
+            // 
+            // ClearButton
+            // 
+            resources.ApplyResources(this.ClearButton, "ClearButton");
+            this.ClearButton.Name = "ClearButton";
+            this.ClearButton.TabStop = false;
+            this.ClearButton.UseVisualStyleBackColor = true;
+            // 
+            // EqualsButton
+            // 
+            resources.ApplyResources(this.EqualsButton, "EqualsButton");
+            this.EqualsButton.Name = "EqualsButton";
+            this.EqualsButton.TabStop = false;
+            this.EqualsButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Result
+            // 
+            resources.ApplyResources(this.textBox_Result, "textBox_Result");
+            this.textBox_Result.Name = "textBox_Result";
+            this.textBox_Result.ReadOnly = true;
+            // 
+            // MainInterface
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.Controls.Add(this.textBox_Result);
+            this.Controls.Add(this.EqualsButton);
+            this.Controls.Add(this.ClearButton);
+            this.Controls.Add(this.ClearEverythingButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.SubtractButton);
+            this.Controls.Add(this.DivideButton);
+            this.Controls.Add(this.CommaButton);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.MultiplyButton);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainInterface";
+            this.Load += new System.EventHandler(this.MainInterface_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button MultiplyButton;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button CommaButton;
+        private System.Windows.Forms.Button DivideButton;
+        private System.Windows.Forms.Button SubtractButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button ClearEverythingButton;
+        private System.Windows.Forms.Button ClearButton;
+        private System.Windows.Forms.Button EqualsButton;
+        private System.Windows.Forms.TextBox textBox_Result;
+    }
+}
+

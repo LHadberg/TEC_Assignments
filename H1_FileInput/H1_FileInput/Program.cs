c55ab1cee0136ef38c5a76a649d03cd266fd85aa
LHadberg <54304968+LHadberg@users.noreply.github.com>
2019-11-08 11:07:30 +0100

    Add files via upload

---
+++ H1_FileInput/H1_FileInput/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace H1_FileInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sti = @"C:\Users\tec\Documents\TestEnviroment\TestDoc.txt";
+            using (StreamReader sr = File.OpenText(sti))
+            {
+                string datafrafil = "";
+                while ((datafrafil = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine("Data fra filen er: " + datafrafil);
+                }
+                Console.ReadLine();
+            }   
+        }
+    }
+}
